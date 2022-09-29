f7c6792ecdacaf9334c0e6d70dd15d436d41132b
UP210775 <up210775@alumnos.upa.edu.mx>
2022-09-29 13:45:20 -0500

    jkashdkjs

---
+++ abc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    char letra = 65; //letras= 'A'
+
+    for (int i = 0; i < 26; i++)
+    {
+        cout << letra << "\t";
+        letra = letra + 1;
+    }
+    return 0;
+}
